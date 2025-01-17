b54f60db7229d00bb064aead36ea056417d0139e
abdullah-mallah <abdullahmallah2000@gmail.com>
2025-01-17 17:30:35 +0100

    created intefaces for login, sign up and account details tab in the main window

---
+++ program/controller/main.py
@@ -0,0 +1,15 @@
+import os
+import sys
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..')))
+from PyQt5.QtWidgets import QApplication
+from program.view.login import UI_login_window
+
+
+def main():
+    app = QApplication(sys.argv)
+    main_window = UI_login_window()
+    app.exec_()
+
+
+if __name__ == "__main__":
+    main()
---
+++ program/view/login.py
@@ -0,0 +1,70 @@
+import sys
+import os
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..')))
+from PyQt5.QtWidgets import *
+from PyQt5 import uic
+from PyQt5.QtCore import *
+from program.view.sign_up import UI_signup_window
+from program.view.main_window import UI_main_window
+
+
+class UI_login_window(QMainWindow):
+    def __init__(self, parent=None):
+        super(UI_login_window, self).__init__(parent)
+        uic.loadUi(os.path.join(os.path.dirname(__file__), "ui_files", "login_page.ui"), self)
+
+        # accessing widgets
+        self.btn_login = self.findChild(QPushButton, "btn_login")
+
+        self.txt_user_id = self.findChild(QLineEdit, "line_edit_user_name")
+        self.txt_password = self.findChild(QLineEdit, "line_edit_password")
+
+        self.lbl_wrong_input = self.findChild(QLabel, "lbl_wrong_credentials")
+        self.lbl_signup = self.findChild(QLabel, "lbl_signup")
+
+        # actions
+        self.btn_login.clicked.connect(self.btn_login_clicked)
+
+        # Make lbl_signup clickable
+        self.lbl_signup.mousePressEvent = self.lbl_signup_clicked
+        
+        self.show()
+
+    def btn_login_clicked(self):
+      self.wrong_inputs = False
+      self.check_input()
+      if not self.wrong_inputs:
+            self.UI_main_window = UI_main_window(self, self.user_id)
+            self.UI_main_window.signal_object.connect(self.show)
+            self.clear_window()
+            self.close()
+            self.UI_main_window.show()
+
+    def check_input(self):
+        self.get_window_values()
+        forbidden_symbols = ["'", '"', ";", "--", "/*", "*/", "#"]
+        if any(symbol in self.user_id for symbol in forbidden_symbols):
+            self.lbl_wrong_input.setText("No injection symbols allowed")
+            self.wrong_inputs = True
+        elif any(symbol in self.password for symbol in forbidden_symbols):
+            self.lbl_wrong_input.setText("No injection symbols allowed")
+            self.wrong_inputs = True
+        if not self.controller.authenticate_user(self.user_id, self.password):
+            self.lbl_wrong_input.setText("Wrong credentials")
+            self.wrong_inputs = True
+
+    def get_window_values(self):
+        self.user_id = self.txt_user_id.text()
+        self.password = self.txt_password.text()
+
+    def clear_window(self):
+        self.txt_user_id.clear()
+        self.txt_password.clear()
+        self.lbl_wrong_input.setText("")
+
+    def lbl_signup_clicked(self, event=None): # Accept event for mousePressEvent
+        self.UI_singup_window = UI_signup_window(self)
+        self.UI_singup_window.signal_object.connect(self.show)
+        self.clear_window()
+        self.close()
+        self.UI_singup_window.show()
